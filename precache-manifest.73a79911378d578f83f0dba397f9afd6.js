a09b76fb5ef19f4569469ee454d0896505dfabd7
Boldbaatar80 <sysupgroup@gmail.com>
2020-04-06 12:31:49 +0800

    Updates

---
+++ precache-manifest.73a79911378d578f83f0dba397f9afd6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19dd7ab97ffc40527b8d3c4c094e82a4",
+    "url": "/burger/index.html"
+  },
+  {
+    "revision": "d43d398ed830c774ba12",
+    "url": "/burger/static/css/3.97fed7e4.chunk.css"
+  },
+  {
+    "revision": "1038400bd81548cbaf19",
+    "url": "/burger/static/css/4.2808e02d.chunk.css"
+  },
+  {
+    "revision": "4da9bf533849fff1489f",
+    "url": "/burger/static/css/5.9db0b95f.chunk.css"
+  },
+  {
+    "revision": "1a5deb572ec69f16de1c",
+    "url": "/burger/static/css/main.ebe3aaf8.chunk.css"
+  },
+  {
+    "revision": "f6c54d10d8a4c29406d8",
+    "url": "/burger/static/js/2.f2ef8a46.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/burger/static/js/2.f2ef8a46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d43d398ed830c774ba12",
+    "url": "/burger/static/js/3.3370447d.chunk.js"
+  },
+  {
+    "revision": "1038400bd81548cbaf19",
+    "url": "/burger/static/js/4.5d262a44.chunk.js"
+  },
+  {
+    "revision": "4da9bf533849fff1489f",
+    "url": "/burger/static/js/5.db7c04d6.chunk.js"
+  },
+  {
+    "revision": "1a5deb572ec69f16de1c",
+    "url": "/burger/static/js/main.f2f9237e.chunk.js"
+  },
+  {
+    "revision": "c93715e722631349c787",
+    "url": "/burger/static/js/runtime-main.ae71c214.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger/static/media/burger-logo.b8503d26.png"
+  }
+]);
